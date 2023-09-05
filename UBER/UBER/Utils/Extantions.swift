336c37e5e5a4498b35b1c1884f1c0b51f33a0ce7
Dilshod Qulmirzayev <dilshodqulmirzayev21@gmail.com>
2023-09-05 23:39:49 +0500

    Adjusted  Consentrets

---
+++ UBER/UBER/Utils/Extantions.swift
@@ -0,0 +1,52 @@
+//
+//  Extantions.swift
+//  UBER
+//
+//  Created by MacBook Pro on 05/09/23.
+//
+
+import UIKit
+
+extension UIView {
+    func ancher(top: NSLayoutYAxisAnchor? = nil ,
+                left: NSLayoutXAxisAnchor? = nil,
+                right: NSLayoutXAxisAnchor? = nil,
+                bottom: NSLayoutYAxisAnchor? = nil,
+                paddingTop: CGFloat = 0 ,
+                padingLeft: CGFloat = 0 ,
+                paddingBottom: CGFloat = 0 ,
+                paddingRight: CGFloat = 0 ,
+                width: CGFloat? = nil,
+                height: CGFloat? = nil) {
+        
+        translatesAutoresizingMaskIntoConstraints = false
+        if let top = top {
+            topAnchor.constraint(equalTo: top, constant: paddingTop).isActive = true
+        }
+        if let left = left {
+            leftAnchor.constraint(equalTo: left, constant: padingLeft).isActive = true
+        }
+        if let right = right {
+            rightAnchor.constraint(equalTo: right, constant: -paddingRight).isActive = true
+        }
+        
+        if let bottom = bottom {
+            bottomAnchor.constraint(equalTo: bottom, constant: -paddingBottom).isActive = true
+        }
+        
+        if let width = width {
+            widthAnchor.constraint(equalToConstant: width)
+        }
+        
+        if let height = height {
+            heightAnchor.constraint(equalToConstant: height)
+        }
+    }
+    
+    func centerX(inView view: UIView ) {
+        centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+    }
+    func centerY(inView view: UIView) {
+        centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+    }
+}
